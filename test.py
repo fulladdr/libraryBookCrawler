09e27cd63812a49a4ab4948f0dae95a15f4da80a
jeyme <jeymemoon@gmail.com>
2020-03-13 23:14:14 +0900

    add file

---
+++ test.py
@@ -0,0 +1,15 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import urllib.request
+from urllib import parse
+
+title = input("책 제목을 입력해주세요 : ")
+
+html_tmpStart= "https://library.sogang.ac.kr/searchTotal/result?st=KWRD&si=TOTAL&q="
+html = urllib.parse.quote_plus(str(title))
+html_tmpEnd = "&x=0&y=0"
+FullURL = html_tmpStart + html + html_tmpEnd
+FullHTML = urllib.request.urlopen(FullURL)
+bsObject = BeautifulSoup(FullHTML, "html.parser")
+
+print(bsObject)

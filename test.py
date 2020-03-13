3bc4222f317d4b1d708729fb3eb636c8b3665cf7
jeyme <jeymemoon@gmail.com>
2020-03-13 23:27:46 +0900

    parsing succeeded

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ title = input("책 제목을 입력해주세요 : ")
 html_tmpStart= "https://library.sogang.ac.kr/searchTotal/result?st=KWRD&si=TOTAL&q="
 html = urllib.parse.quote_plus(str(title))
 html_tmpEnd = "&x=0&y=0"
-FullURL = html_tmpStart + html + html_tmpEnd'maketotalURL'
+FullURL = html_tmpStart + html + html_tmpEnd
 FullHTML = urllib.request.urlopen(FullURL)
 bsObject = BeautifulSoup(FullHTML, "html.parser")
 

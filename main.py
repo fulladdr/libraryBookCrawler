53e2a01c3a300c34764ee477b789f0bf3f448a71
jeyme <jeymemoon@gmail.com>
2020-03-15 01:08:53 +0900

    Added Function

--- main.py
+++ main.py
@@ -4,35 +4,40 @@ from operator import eq
 import urllib.request
 from urllib import parse
 
-num = 0
-flag=0
-
 def findFunction() :
     global num
     global flag
     titleBook = titleParsing[num].find('a').text
-    if titleBook==title:
-        print("제목 : ", titleBook)
-        location = locationParsing[num].find('a').text
-        wh = 0 
-        while location[wh]!="]":
-            wh=wh+1
-        print("위치 : ")
-        numList = list(range(0,wh+1))
-        for j in numList:
-            print(location[j], end = '')
-        print("\n")
-        flag = 1
+    print("%d." % (num+1))
+    print("제목 : ", titleBook)
+    location = locationParsing[num].find('a').text
+    wh = 0 
+    while location[wh]!="]":
+        wh=wh+1
+    print("위치 : ", end='')
+    numList = list(range(0,wh+1))
+    for j in numList:
+        print(location[j], end = '')
+    flag = 1
+    detail = detailParsing[num].find('span').text
+    if detail=="상세정보확인":
+        print("\n대출가능")
+    else:
+        print("\n대출불가능")
+    print("\n") 
 
-def solve() :
+def callFunction() :
     global num
-    while num < len(locationParsing):
+    length = min(len(locationParsing), len(titleParsing))
+    while num < length:
         findFunction()
         num = num+1
     if flag==0 :
         print("nothing has been found\n")
 
 if __name__ == "__main__":
+    num=0
+    flag=0
     title = input("책 제목을 입력해주세요 : ")
     html_tmpStart= "https://library.sogang.ac.kr/searchTotal/result?st=KWRD&si=TOTAL&q="
     html = urllib.parse.quote_plus(str(title))
@@ -42,6 +47,5 @@ if __name__ == "__main__":
     bsObject = BeautifulSoup(FullHTML, "html.parser")
     titleParsing = bsObject.findAll('p', attrs={'class':'listTitle'})
     locationParsing = bsObject.findAll('p', attrs={'class':'location'})
-    detailParsing = bsObject.findAll('div', {'class':'footable-row-detail-inner'})
-    solve()
-    
+    detailParsing =  bsObject.findAll('p', attrs={'class':'location'})
+    callFunction()
